31066a9cbf58a7ee370820f4f94a0d394833a35c
Ameen <ameenaladdin@gmail.com>
2023-08-14 21:00:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_cors import CORS
 import requests
 
@@ -6,10 +6,13 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/', methods=['GET'])
-def proxy():
-    r = requests.get('http://nginxseatalert.duckdns.org', params=request.args)
-    return r.text
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>', methods=['GET'])
+def proxy(path):
+    url = 'http://nginxseatalert.duckdns.org/' + path
+    params = request.args
+    r = requests.get(url, params=params)
+    return Response(r.text, status=r.status_code, content_type=r.headers['content-type'])
 
 
 if __name__ == '__main__':
